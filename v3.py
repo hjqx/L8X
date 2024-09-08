95c1249c1e46d963062cd516239c77ba29d7c397
hjqx <jdjzi8287edh9y2dhd2y28282@mail.ru>
2024-09-08 15:15:47 +0200

    Add files via upload

---
+++ v3.py
@@ -0,0 +1,116 @@
+e="""
+[ L8X obf project ]
+
+    made by zqh 
+    in 1~2 days 
+    v3 run on py 3.10 or +
+
+my social media :
+    discord : hjqk (only active week-end)
+    t.me/zqhbv
+    guns.lol/zqh
+    github.com/hjqx
+
+ur goal :
+    try re the src code
+
+difficulty : [30/100]
+
+good luck 
+"""
+
+_0x738e82,cds={
+    "_0x732f": [0x5, 0x1, 0x0, 0x13],
+    "_0x373d": "_0x46z92",
+    "_0x632q3": hex,
+    "_0x737r": "print",
+    "_0x843df4": __builtins__,
+}, range
+
+def _0x7837d(_0x77d37, _0x28d73, ____):
+    """
+    Control flow flattening 
+       {    
+           match case statements, while, conditions, and for loop
+           try except
+           #TODO add recursive func
+       }
+    spam lambda expressions
+    define functions ( max 3 arg )
+    dict[str, Any],list[int],built func
+    renaming variables
+    no exec eval func uses
+    #TODO redefine builtin functions
+    """
+    def _0x7e8d3(_0x7d83dx):
+     return(_0x77d37)(_0x7d83dx)
+
+    def _0x6738de(*d,**a):
+     for(__)in(cds)(len(''.__class__(a.__class__))):
+      sZ9 =(str().__class__())
+     for dç in d:sZ9+=dç
+     return(sZ9)
+    
+    def _0x3dj83(_0x6367,f,s):
+     __,g=_0x6367,_0x28d73
+     while __!=0x0:__-=____
+     else:r=[]
+     for _0x87d23 in (cds)(f, s):r+=[_0x87d23]
+     return(r, g, __)
+    _0x38d8e2 = (___ := lambda r :____>>____)(_0x77d37)
+    while(().__class__(_0x78d73:=_0x3dj83(0x3f, ____, 0x5))and(_0x38d8e2<____)):
+        def _0x3j3793(c, s, r):
+            while (s<=____)and(_0x78d73[____]==_0x28d73): 
+                _0x388e74,_1_1=0x190,0x183
+                for(IlIl)in(cds)(len(c)):
+                    while(IlIl<____)and(_1_1<=_0x388e74):
+                      IlIl += ____; _1_1 -= 0x1
+                    for(_0x734f38)in(((cds))(_0x388e74)):
+                        match(_0x734f38):
+                        
+                          case(0x16f):
+                              def _0x937de9(g,_0x862rd2, _0x632e2):
+                                  _0x7e8d3(
+                                      _0x6738de(
+                                          _0x738e82["_0x%sd"%"373"[::-0x1]][
+                                              _0x738e82["_0x732f"][_0x632e2]
+                                          ],
+                                          g,
+                                          _0x862rd2,
+                                      )
+                                  )
+
+                          case(0x111):
+                              _0x836e83 = lambda _0x7d37e2, _0x635z8d: _0x937de9(
+                                  [].__class__(_0x738e82.keys())[_0x635z8d][r],
+                                  _0x7d37e2,
+                                  0x0,
+                              )
+                          case(0x176):
+                              _0x737e4 = lambda _0x737e7, __,: _0x836e83(
+                                  _0x737e7, __
+                              )
+                              def e(k,m,s=lambda _,__,___,:''.__class__((_0x738e82)[__])[(
+                                _0x738e82)[___][_]]):k(s(-0x1,"_0x632q3","_0x732f"), m)
+                              v = lambda r , s : e(r, s)
+                              try :v(_0x737e4, 0x2)
+                              except SyntaxError as e:return e
+                s += ____
+        (_0x38d8e2:=_0x38d8e2.__add__(____))
+        def f (z,q,h): return (v := (
+                 lambda now : lambda k,y ,s : now (k,y , s)) # ^^
+                    ( _0x3j3793)) (h,q,z )
+        (lambda e, n ,d : e and f(d,n[-____],n[0x0]))(_0x38d8e2:=
+                _0x38d8e2.__add__(____),_0x78d73,0x6)
+        
+def _0x836dAe2(_9372:_0x7837d):
+    (lambda ___,*f,: _0x7837d(___, (f), 0x1))(_9372, 0x2)
+    return(_9372)
+
+_0x737df = lambda ____: (lambda _0x736rd9: (_0x836dAe2)(_0x736rd9))(
+    ____.__dict__[_0x738e82[[_0x836dAe2].__class__(_0x738e82.keys())[0x3]]]
+)
+
+# VIVE LA FRANCE 
+try:_0x737df(_0x738e82["_0x843df4"]);input()
+except(ValueError):raise("zqh always the goat")
